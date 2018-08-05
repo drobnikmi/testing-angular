d3e3a1441c85782fc17b05377ee2f0a9a36c755f
Ernest Kowalski <ernest@nitro-digital.com>
2018-08-05 23:59:06 +0200

    prettier, prettier config, form builder added

--- src/app/users/user-list/user-modal/user-modal.component.ts
+++ src/app/users/user-list/user-modal/user-modal.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserModel } from '../../models/user.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user-modal',
@@ -10,17 +10,16 @@ export class UserModalComponent implements OnInit {
   user: UserModel;
   profileForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.createForm();
   }
 
   createForm() {
-    this.profileForm = new FormGroup({
-      firstName: new FormControl('Marcin'),
-      lastName: new FormControl('Dabrowski')
+    this.profileForm = this.fb.group({
+      userName: this.fb.control(this.user.name),
+      address: this.fb.control(this.user.address.street)
     });
   }
-
 }
